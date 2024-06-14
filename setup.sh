54eb6b6ce8207a3c09ba2e22b9777ce2f048a200
nleea <neldecas12@gmail.com>
2024-06-14 16:57:13 -0500

    setup update

--- setup.sh
+++ setup.sh
@@ -2,12 +2,18 @@
 
 # Obtener la ruta absoluta del directorio donde se encuentra este script
 SCRIPT_DIR=$(dirname $(readlink -f $0))
+REPO_URL="https://github.com/nleea/wh-auto.git"
+BRANCH="main"
 
-# Navegar al directorio del proyecto
-cd $SCRIPT_DIR
-
-# Actualizar el repositorio
-git pull origin main
+# Verifica si el directorio del proyecto existe
+if [ ! -d "$SCRIPT_DIR" ]; then
+    echo "El directorio no existe. Clonando el repositorio..."
+    git clone -b $BRANCH $REPO_URL $SCRIPT_DIR
+else
+    echo "El directorio existe. Actualizando el repositorio..."
+    cd $SCRIPT_DIR
+    git pull origin $BRANCH
+fi
 
 # Opcional: Crear y activar un entorno virtual
 python3 -m venv app1
